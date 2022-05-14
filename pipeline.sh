0be6e992381dd15b367fb2732c0a731aedce3b26
skelmis <ethan@koldfusion.xyz>
2022-05-15 00:13:50 +1200

    OMPX341-22A-A3 fixing compile-time error

--- pipeline.sh
+++ pipeline.sh
@@ -1,8 +1,13 @@
+if [ "$#" -eq  "0" ] ; then
+    echo 'Please provide a commit message.'
+    exit 1
+fi
+
 cd ./assets
 npm install
 
 if npm run build; then
-  git commit -m "COMPX341-22A-A3 Commiting from CI/CD Pipeline"
+  git commit -m "$1"
 else
   echo 'Failed to build, did not commit.'
   exit 1
