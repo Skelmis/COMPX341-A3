3be22df67b40c48766b0659dfca56d2863ea8134
skelmis <ethan@koldfusion.xyz>
2022-05-14 19:54:32 +1200

    Revert invalid syntax change.

--- assets/src/modules/bestSellers/bestSellersBar/BestSellersBar.tsx
+++ assets/src/modules/bestSellers/bestSellersBar/BestSellersBar.tsx
@@ -13,24 +13,25 @@ import umami from "../../../images/bestSellers/umami.png";
 const bestSellers = [burgers, italian, noodles, pancakes, pineapple, umami];
 
 export class BestSellersBar extends React.Component {
-  render() {
-    return (
-      <div className="center ad-gallery nav">
-        <div className="col-md-2 hidden-sm hidden-xs">
-          <LinkContainer to="/best">
-            <NavItem><h3>Bookstore<br/>Best Sellers</h3></NavItem>
-          </LinkContainer>
-        </div>
-        <div className="row">
-          {bestSellers.map(book =>
-            <div className="col-md-2 hidden-sm hidden-xs" key={book}>
-              <LinkContainer to="/best">
-                <NavItem><img src={book} className="thumbs" /></NavItem>
-              </LinkContainer>
-            </div>)}
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="center ad-gallery nav">
+                <div className="col-md-2 hidden-sm hidden-xs">
+                    <LinkContainer to="/best">
+                        <NavItem><h3>Bookstore<br/>Best Sellers</h3></NavItem>
+                    </LinkContainer>
+                </div>
+                <div className="row">
+                    {bestSellers.map(book =>
+                        <div className="col-md-2 hidden-sm hidden-xs" key={book}>
+                            <LinkContainer to="/best">
+                                <NavItem><img src={book} className="thumbs"/></NavItem>
+                            </LinkContainer>
+                        </div>)}
+                </div>
+            </div>
+        );
+    }
+}
 
 export default BestSellersBar;
